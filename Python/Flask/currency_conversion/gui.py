a3027d1cc51be927a7482e0557842d5664bfc0d8
Saurav Dwivedi <saurav.dwivedi@gmail.com>
2023-01-09 00:38:18 +0100

    Update gui.py

--- Python/Flask/currency_conversion/gui.py
+++ Python/Flask/currency_conversion/gui.py
@@ -3,7 +3,7 @@
 import tkinter as tk
 import datetime
 import logging
-from backend import CurrencyConversionAPI
+from backend import CurrencyConversionAPI, CurrencyConversionScheduler
 
 
 logging.getLogger(__name__)
@@ -11,10 +11,14 @@ logging.getLogger(__name__)
 
 class Model:
     def __init__(self) -> None:
-        global rate, time
+        global rate, time, next_execution
         rate, time = self.api_call()
         self.rate = rate
         self.time = time
+        self.schedule = CurrencyConversionScheduler(
+            scheduler_enabled=True, scheduler_cron="0 11 * * *"
+        )
+        next_execution = self.schedule.next_execution
 
     def api_call(self) -> tuple:
         result: dict = CurrencyConversionAPI().get()
@@ -48,6 +52,11 @@ class View(tk.Frame):
 
         self.time_label = tk.Label(master=self.frame_label, text=f"at {time}")
         self.time_label.pack()
+
+        self.next_ex_label = tk.Label(
+            master=self.frame_label, text=f"Next execution: {next_execution}"
+        )
+        self.next_ex_label.pack()
 
         self.get_button = tk.Button(
             master=self.frame_buttons,
@@ -87,19 +96,27 @@ class Controller:
         self.view = view
 
     def refresh_button_clicked(self):
-        rate, time = self.model.api_call()
-        self.view.rate_label["text"] = f"1 SEK = {rate} INR"
-        self.view.time_label["text"] = f"at {time}"
+        self.view.next_ex_label[
+            "text"
+        ] = f"Next execution: {self.model.schedule.next_execution}"
+
+        if self.model.schedule.result:
+            self.view.rate_label[
+                "text"
+            ] = f"1 SEK = {self.model.schedule.result.get('quote')} INR"
+            self.view.time_label[
+                "text"
+            ] = f"at {datetime.datetime.fromtimestamp(self.model.schedule.result.get('timestamp'))}"
 
     def close_application(self):
-        exit()
+        raise SystemExit
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("SKE to INR conversion rate")
-        self.geometry("300x150")
+        self.geometry("350x150")
 
         model: Model = Model()
         view: View = View(self)
