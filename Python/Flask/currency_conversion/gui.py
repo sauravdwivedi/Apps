3226baea4431b2849759eda54a9c41069fe4b2cf
Saurav Dwivedi <saurav.dwivedi@gmail.com>
2023-01-08 23:29:09 +0100

    Update gui.py

--- Python/Flask/currency_conversion/gui.py
+++ Python/Flask/currency_conversion/gui.py
@@ -2,7 +2,6 @@
 
 import tkinter as tk
 import datetime
-from functools import partial
 import logging
 from backend import CurrencyConversionAPI
 
@@ -10,70 +9,109 @@ from backend import CurrencyConversionAPI
 logging.getLogger(__name__)
 
 
-def api_call() -> tuple:
-    result: dict = CurrencyConversionAPI().get()
+class Model:
+    def __init__(self) -> None:
+        global rate, time
+        rate, time = self.api_call()
+        self.rate = rate
+        self.time = time
 
-    if result.get("info"):
-        rate: str = result.get("info").get("quote")
-        time: str = datetime.datetime.fromtimestamp(result.get("info").get("timestamp"))
-        logging.info(f"SEK to INR at {time}: {rate}")
-    else:
-        rate: str = "NULL"
-        time: str = "NULL"
-        logging.error(f"An error ocurred in API call!")
+    def api_call(self) -> tuple:
+        result: dict = CurrencyConversionAPI().get()
 
-    return rate, time
+        if result.get("info"):
+            rate: str = result.get("info").get("quote")
+            time: str = datetime.datetime.fromtimestamp(
+                result.get("info").get("timestamp")
+            )
+            logging.info(f"SEK to INR at {time}: {rate}")
+        else:
+            rate: str = "NULL"
+            time: str = "NULL"
+            logging.error(f"An error ocurred in API call!")
+
+        return rate, time
 
 
-def refresh_api_call(rate_label: str, rate_time: str) -> None:
-    rate, time = api_call()
-    rate_label["text"] = f"1 SEK = {rate} INR"
-    rate_time["text"] = f"at {time}"
+class View(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.frame_label = tk.Frame(relief=tk.SUNKEN, borderwidth=0)
+        self.frame_label.grid(row=0, column=0, padx=5, pady=5)
+
+        self.frame_buttons = tk.Frame(relief=tk.RIDGE, borderwidth=0)
+        self.frame_buttons.grid(row=0, column=1, padx=5, pady=5)
+
+        self.rate_label = tk.Label(master=self.frame_label, text=f"1 SEK = {rate} INR")
+        self.rate_label.pack()
+
+        self.time_label = tk.Label(master=self.frame_label, text=f"at {time}")
+        self.time_label.pack()
+
+        self.get_button = tk.Button(
+            master=self.frame_buttons,
+            width=5,
+            text="Refresh",
+            fg="green",
+            command=self.refresh_button_clicked,
+        )
+        self.get_button.grid(row=0, column=0)
+
+        self.close_button = tk.Button(
+            master=self.frame_buttons,
+            text="Close",
+            width=5,
+            fg="red",
+            command=self.close_application,
+        )
+        self.close_button.grid(row=1, column=0)
+
+        self.controller = None
+
+    def set_controller(self, controller):
+        self.controller = controller
+
+    def refresh_button_clicked(self):
+        if self.controller:
+            self.controller.refresh_button_clicked()
+
+    def close_application(self):
+        if self.controller:
+            self.controller.close_application()
 
 
-def sek_to_inr(main_window: tk.Tk, rate: str, time: str) -> None:
-    main_window.title("SKE to INR conversion rate")
-    main_window.geometry("300x150")
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
 
-    frame_label = tk.Frame(master=main_window, relief=tk.SUNKEN, borderwidth=0)
-    frame_label.grid(row=0, column=0, padx=5, pady=5)
+    def refresh_button_clicked(self):
+        rate, time = self.model.api_call()
+        self.view.rate_label["text"] = f"1 SEK = {rate} INR"
+        self.view.time_label["text"] = f"at {time}"
 
-    frame_buttons = tk.Frame(master=main_window, relief=tk.RIDGE, borderwidth=0)
-    frame_buttons.grid(row=0, column=1, padx=5, pady=5)
+    def close_application(self):
+        exit()
 
-    rate_label = tk.Label(master=frame_label, text=f"1 SEK = {rate} INR")
-    rate_label.pack()
 
-    time_label = tk.Label(master=frame_label, text=f"at {time}")
-    time_label.pack()
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("SKE to INR conversion rate")
+        self.geometry("300x150")
 
-    refresh_api_call_with_args = partial(refresh_api_call, rate_label, time_label)
+        model: Model = Model()
+        view: View = View(self)
+        view.grid(row=0, column=0)
+        controller: Controller = Controller(model, view)
 
-    get_button = tk.Button(
-        master=frame_buttons,
-        width=5,
-        text="Refresh",
-        fg="green",
-        command=refresh_api_call_with_args,
-    )
-    get_button.grid(row=0, column=0)
-
-    close_button = tk.Button(
-        master=frame_buttons,
-        text="Close",
-        width=5,
-        fg="red",
-        command=main_window.destroy,
-    )
-    close_button.grid(row=1, column=0)
-
-    main_window.mainloop()
+        view.set_controller(controller)
 
 
 def main() -> None:
-    main_window: tk.Tk = tk.Tk()
-    result: tuple = api_call()
-    sek_to_inr(main_window, *result)
+    app: App = App()
+    app.mainloop()
 
 
 if __name__ == "__main__":
